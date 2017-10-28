8c52ad550011e1bedf85d632525d1a4e89c04d6c
m3lc <melas.christos@gmail.com>
2017-10-28 12:39:16 +0300

    - added video - menu animation - updated some dimensions to be proportional to viewport's width

--- src/js/MainView.js
+++ src/js/MainView.js
@@ -1,3 +1,12 @@
 export default class MainView {
-
+    init($) {
+        $('nav a').click(function(evn){
+            evn.preventDefault();
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 700, () => {
+                window.location.hash = this.hash;
+            });
+        });
+    }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import styles from '../styles/styles.scss';//eslint-disable-line
 import MainView from './MainView';
 
 jQuery(function($) { //eslint-disable-line
-    const mainView = new MainView(); //eslint-disable-line
+    const mainView = new MainView();
+    mainView.init($);
 });
