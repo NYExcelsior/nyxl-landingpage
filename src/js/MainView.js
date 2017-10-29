3a4f053fbf07c9637a2390810940b711e1722700
m3lc <melas.christos@gmail.com>
2017-10-29 22:30:26 +0200

    - updated players' details - added overlay for social icons - updated footer, logo matches text in width

--- src/js/MainView.js
+++ src/js/MainView.js
@@ -1,11 +1,21 @@
 export default class MainView {
     init($) {
-        $('nav a').click(function(evn){
-            evn.preventDefault();
+        $('nav a').click(function(e){
+            e.preventDefault();
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top
             }, 700, () => {
                 window.location.hash = this.hash;
+            });
+        });
+        $('.email .email__form-submit input').on('click', function(e) {
+            e.preventDefault();
+            $.ajax({
+                url:$('.email .email__form form').action,
+                type:'post',
+                data:$('.email .email__form form').serialize(),
+                success:function(){
+                }
             });
         });
     }
