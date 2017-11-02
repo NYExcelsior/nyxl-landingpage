5284e568acfde5fe1fe5d98bf531e4e59eb061d9
m3lc <melas.christos@gmail.com>
2017-11-02 07:02:22 +0200

    - made menu sticky - updating url on each section

--- src/js/MainView.js
+++ src/js/MainView.js
@@ -1,3 +1,5 @@
+import Waypoints from '../../node_modules/waypoints/lib/jquery.waypoints.js';
+import Inview from '../../node_modules/waypoints/lib/shortcuts/inview.js';
 export default class MainView {
     init($) {
         $('nav a').click(function(e){
@@ -6,6 +8,31 @@ export default class MainView {
                 scrollTop: $(this.hash).offset().top
             }, 700, () => {
                 window.location.hash = this.hash;
+            });
+        });
+        $('section').each(function() {
+            let inview = new Waypoint.Inview({
+                element: this,
+                entered: function() {
+                    let sectionId = $(this.element).attr('id');
+                    if(sectionId) {
+                        if(!~window.location.hash.indexOf(sectionId)) {
+                            if(history.pushState) {
+                                history.replaceState({},'',`#${sectionId}`);
+                            } else {
+                                $(this.element).attr('id', '');
+                                window.location.hash = sectionId;
+                                $(this.element).attr('id', sectionId);inview.destroy();
+                            }
+                        }
+                    } else {
+                        if(history.pushState) {
+                            history.replaceState({}, '', '/');
+                        } else {
+                            window.location.hash = '';
+                        }
+                    }
+                }
             });
         });
         $('.roster .roster__row').on('click', function() {
@@ -34,5 +61,12 @@ export default class MainView {
             $('body').toggleClass('popup-menu-active');
             $('.menu .menu-bars').toggleClass('is-active');
         });
+        document.ontouchmove = function (e) {
+            if($('body').hasClass('popup-menu-active')) {
+                e.preventDefault();
+            } else {
+                return true;
+            }
+        };
     }
 }
